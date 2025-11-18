a0497b7db5d77336b06308bd42b931aa00b34cca
JosueAlvarezAvendano <josue.avendano@sptech.school>
2025-11-18 15:22:11 -0300

    Integrando API ao projeto

---
+++ banco/Script - Banco de Dados.sql
@@ -0,0 +1,18 @@
+-- SCRIPT DO BANDO DE DADOS - JACH1A AJAYU
+
+-- Criando o Database Jach'a Ajayu
+CREATE DATABASE jacha_ajayu;
+USE jacha_ajayu;
+
+CREATE TABLE Usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	sobrenome VARCHAR(45),
+	email VARCHAR(60),
+	senha VARCHAR(20)
+);
+
+SELECT * FROM usuario;
+
+DROP TABLE usuario;
+DROP DATABASE jacha_ajayu;
